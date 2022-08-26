3f6bf09d8c58918b8dde3b8d06ac75a3f4c1e934
Vu Bui Khanh Linh <73817943+Linh-0v0@users.noreply.github.com>
2022-08-26 14:03:47 +0700

    Initial Commit

---
+++ DealSearch/DealSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DealSearchApp.swift
+//  DealSearch
+//
+//  Created by Vu Bui Khanh Linh on 26/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct DealSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
